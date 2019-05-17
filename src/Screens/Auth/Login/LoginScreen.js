28986dbd92143ad38982d950f489184b5b82d69b
Khoi Vu <vuminhkhoi1802@gmail.com>
2019-05-17 17:58:35 +0700

    updated Login Screen Background

--- src/Screens/Auth/Login/LoginScreen.js
+++ src/Screens/Auth/Login/LoginScreen.js
@@ -8,10 +8,16 @@ import {
 	AsyncStorage,
 	ActivityIndicator,
 	Image,
-	StatusBar
+	StatusBar,
+	ImageBackground,
+	Dimensions
 } from 'react-native';
 import { tsConstructorType } from '@babel/types';
+import bgImage from '../../../../assets/images/background.jpg';
 
+const {
+	width: WIDTH
+} = Dimensions.get('window')
 export default class LoginScreen extends React.Component {
 	static navigationOptions = () => ({
 		title: 'Login'
@@ -31,4 +37,51 @@ export default class LoginScreen extends React.Component {
 		if (!TextInputUser) {
 		}
 	};
+
+	render() {
+		return (
+			<ImageBackground source={bgImage} style={styles.backgroundContainer}>
+				<View style={{ flex: 1 }}>
+					<TextInput
+						style={styles.input}
+						placeholder={'Username'}
+						placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
+						underlineColorAndroid='transparent'
+					/>
+				</View>
+			</ImageBackground>
+		);
+	}
 }
+
+const styles = StyleSheet.create({
+	backgroundContainer: {
+		flex: 1,
+		width: null,
+		height: null,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+
+	logoContainer: {
+		alignItems: 'center'
+	},
+
+	logoText: {
+		color:'white',
+		fontSize: 20,
+		fontWeight: '500',
+		marginTop: 10,
+		opacity: 0.5
+	},
+	input: {
+		width: WIDTH - 55,
+		height: 25,
+		borderRadius: 25,
+		fontSize: 16,
+		paddingLeft: 45,
+		backgroundColor: 'rgba(0,0,0,0.35)'
+		
+
+	}
+});
